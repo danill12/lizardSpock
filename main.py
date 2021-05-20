a9ef904381ea2fdd56b1469d4fe1e87504513061
Carlos <carmesce@gmail.com>
2021-05-20 20:00:53 +0200

    revisión con el profe

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 # Piedra, papel, tijera, lagarto o spock
 
-
 import random
 import json
 import os
 import time
 from mensajes import msg
 
-SAVEFILE = "puntuacion2.txt"
+SAVEFILE = "save.json"
 SAVE_ON_EXIT = True
 SAVE_ON_CYCLE = True
-
-# definimos los movimientos del juego y los elementos de control para manejarnos por el mismo
-moves = ["R", "P", "S", "L", "K"]
-movesM = ["r", "p", "s", "l", "k"]
-control = ["E", "RQ"]
-controlM = ["e", "rq"]
+# definimos los movimientos del juego y los elementos de CONTROL para manejarnos por el mismo
+MOVES = ["R", "P", "S", "L", "K"]
+WINMOVES = [["R", "S"], ["R", "L"], ["P", "R"], ["P", "K"], ["S", "P"], ["S", "L"], ["L", "K"], ["L", "P"], ["K", "S"], ["K", "R"]]
+CONTROL = ["E"]
 
 gameStatus = None
 
 
 def jugador(gameStatus):
     # Introducimos el nombre del usuario y verificamos si ya esta guardado, si no creamos una nueva "partida"
-    plyr = input("Introduzca su nombre: ")
+    plyr = input(msg["nameInput"])
     if plyr not in gameStatus["punt"]:
         gameStatus["punt"][plyr] = newPun()
         print(msg["crear"])
@@ -32,40 +29,40 @@ def jugador(gameStatus):
     return plyr
 
 
-def inicio(moves, control):
+def getPlayerMove():
     # Inciamos el juego y pedimos los movimientos del jugador, tambien comprobamos si son correctos
-    usuario = introducir()
-    while usuario not in moves + control and usuario not in movesM + controlM:
+    def getPlayerInput():
+        # funcion input para pedir los movimientos del jugador
+        elecc = input(msg["getPlayerMove"]).upper()
+        increaseInputCounter(gameStatus)
+
+        return elecc
+
+    usuario = getPlayerInput()
+    while usuario not in MOVES + CONTROL:
         print(msg["error"])
         print(msg["exit"])
-        usuario = introducir()
-    contadorBuenas(gameStatus)
+        usuario = getPlayerInput()
+    increaseGoodCounter(gameStatus)
+
     return usuario
 
 
-def introducir():
-    # funcion input para pedir los movimientos del jugador
-    elecc = input(msg["input"])
-    contadorInput(gameStatus)
-    return elecc
-
-
-def juego(usuario, gameStatus, startTime, moves, control):
+def juego(usuario, gameStatus, startTime):
     # funcion juego es donde esta la logica del programa
     # la maquina elige un movimiento
-    maquina = random.choice(moves)
-
-    win = [["R", "S"], ["R", "L"], ["P", "R"], ["P", "K"], ["S", "P"], ["S", "L"], ["L", "K"], ["L", "P"], ["K", "S"], ["K", "R"]]
-    winM = [["r", "s"], ["r", "l"], ["p", "r"], ["p", "k"], ["s", "p"], ["s", "l"], ["l", "k"], ["l", "p"], ["k", "s"], ["k", "r"]]
+    maquina = random.choice(MOVES)
 
     # sistema de salida del juego con la tecla "E" o "e"
-    if usuario in control or usuario in controlM:
+    if usuario in CONTROL:
         # llama a la funcion contadorTiempo para obtener el timpo de juego
-        gameStatus = contadorTiempo(gameStatus, startTime)
+
+
         if SAVE_ON_EXIT:
             guardar(gameStatus)
+
         print(msg["mensajeSalida"])
-        imprimir(gameStatus)
+        printStats(startTime)
         exit()
 
     # mejora de interfaz al indicar la eleccion de la maquina y la jugada
@@ -76,17 +73,11 @@ def juego(usuario, gameStatus, startTime, moves, control):
         "S": msg["moveS"],
         "L": msg["moveL"],
         "K": msg["moveK"],
-
-        "r": msg["moveR"],
-        "p": msg["moveP"],
-        "s": msg["moveS"],
-        "l": msg["moveL"],
-        "k": msg["moveK"]
     }
 
     eleccion = {
-        "eleccionUsuario": opcionesMovimientos[usuario],
-        "eleccionMaquina": opcionesMovimientos[maquina]
+        "userChoice": opcionesMovimientos[usuario],
+        "cpuChoice": opcionesMovimientos[maquina]
     }
 
     print(msg["mensajeMaquina"].format(**eleccion))
@@ -96,7 +87,7 @@ def juego(usuario, gameStatus, startTime, moves, control):
         print(msg["mensajeEmpate"].format(**eleccion))
         punto = 0
         return punto
-    elif [usuario, maquina] in win or [usuario, maquina] in winM:
+    elif [usuario, maquina] in WINMOVES:
         print(msg["mensajeVictoria"].format(**eleccion))
         punto = 2
         return punto
@@ -109,10 +100,10 @@ def juego(usuario, gameStatus, startTime, moves, control):
 def newPun():
     # funcion utulizada para añadir una partida a los jugadores nuevos
     return {
-        "Partidas": 0,
-        "Victorias": 0,
-        "Derrotas": 0,
-        "Empates": 0,
+        "partidas": 0,
+        "victorias": 0,
+        "derrotas": 0,
+        "empates": 0,
     }
 
 
@@ -129,58 +120,49 @@ def diccionario():
             },
             "stats": {
                 "tiempo_ejec": 0.0,
-                "total_imput": 0,
-                "buenas_imput": 0,
+                "total_input": 0,
+                "buenas_input": 0,
             }
         }
         return gameStatus
 
 
-def tiempoInicio():
-    # iniciamos la cuenta del tiempo
-    startTime = time.time()
-    return startTime
-
-
-def tiempoEjecucion(startTime):
-    # operacion para obtener el tempo de juego
-    elapsedTime = time.time() - startTime
-    return elapsedTime
-
-
 def contadorTiempo(gameStatus, startTime):
     # sumamos el tiempo de la partida actual al tiempo total del programa
-    gameStatus["stats"]["tiempo_ejec"] = gameStatus["stats"]["tiempo_ejec"] + tiempoEjecucion(startTime)
-    return gameStatus
+    sessionTime = time.time() - startTime
+    gameStatus["stats"]["tiempo_ejec"] += sessionTime
+
+    return sessionTime
 
 
 def convertidorTiempo(elapsedTime):
     # mejora visual para sacar el tiempo de juego
     gmTime = time.gmtime(elapsedTime)
     slapsedStr = time.strftime("%H:%M:%S", gmTime)
+
     return slapsedStr
 
 
-def contadorInput(gameStatus):
+def increaseInputCounter(gameStatus):
     # contador de los input totales que entran por teclado
-    gameStatus["stats"]["total_imput"] = gameStatus["stats"]["total_imput"] + 1
+    gameStatus["stats"]["total_input"] += 1
 
 
-def contadorBuenas(gameStatus):
-    # contador de los inputs que el programa acepta como buenos(los movimientos del juego o elementos de control)
-    gameStatus["stats"]["buenas_imput"] = gameStatus["stats"]["buenas_imput"] + 1
+def increaseGoodCounter(gameStatus):
+    # contador de los inputs que el programa acepta como buenos(los movimientos del juego o elementos de CONTROL)
+    gameStatus["stats"]["buenas_input"] += 1
 
 
 def puntuacion(puntos, gameStatus, plyr):
     # suma del resultado de la ronda en el diccionario
-    gameStatus["punt"][plyr]["Partidas"] = gameStatus["punt"][plyr]["Partidas"] + 1
+    gameStatus["punt"][plyr]["partidas"] += 1
 
     if puntos == 0:
-        gameStatus["punt"][plyr]["Empates"] = gameStatus["punt"][plyr]["Empates"] + 1
+        gameStatus["punt"][plyr]["empates"] += 1
     elif puntos == 1:
-        gameStatus["punt"][plyr]["Derrotas"] = gameStatus["punt"][plyr]["Derrotas"] + 1
+        gameStatus["punt"][plyr]["derrotas"] += 1
     else:
-        gameStatus["punt"][plyr]["Victorias"] = gameStatus["punt"][plyr]["Victorias"] + 1
+        gameStatus["punt"][plyr]["victorias"] += 1
 
     # salida de texto con el resltado de la ronda (llamada al archivo mensajes.py)
     print(msg["jugador"].format(**{"plyr": plyr}))
@@ -197,35 +179,29 @@ def guardar(gameStatus):
         json.dump(gameStatus, outfile, indent=4)
 
 
-def imprimir(gameStatus):
-    # llama a la funcion tiempoConvertido e imprime el resultado, a su vez imprime el numeor de outputs(totales y aceptados) (mensajes.py)
-    tiempoConvertido = convertidorTiempo(gameStatus["stats"]["tiempo_ejec"])
-    print(msg["tiempo"].format(**{"tiempoConvertido": tiempoConvertido}))
+def printStats(startTime):
+    # llama a la funcion tiempoTotal e imprime el resultado, a su vez imprime el numeor de outputs(totales y aceptados) (mensajes.py)
+    sessionTime = contadorTiempo(gameStatus, startTime)
+    sessionText = convertidorTiempo(sessionTime)
+
+    print(msg["sessionTime"].format(**{"sessionTime": sessionText}))
+
+    timeText = convertidorTiempo(gameStatus["stats"]["tiempo_ejec"])
+    print(msg["totalTime"].format(**{"tiempoTotal": timeText}))
 
     print(msg["stats"].format(**gameStatus["stats"]))
 
 
-def ranking(usuario, gameStatus):
-    # proyecto de ranking
-    for n in punt:
-        m = n + 1
-        if gameStatus["punt"][n]["Victorias"] > gameStatus["punt"][m]["Victorias"]:
-            print(gameStatus["punt"][n])
-        else:
-            n = n + 1
-
-
 def main():
-    startTime = tiempoInicio()
-    print(msg["inicio"])
     global gameStatus
+
+    startTime = time.time()
+    print(msg["inicio"])
     gameStatus = diccionario()
     plyr = jugador(gameStatus)
     while True:
-        usuario = inicio(moves, control)
-        if usuario == "RQ":
-            ranking(usuario, gameStatus)
-        puntos = juego(usuario, gameStatus, startTime, moves, control)
+        usuario = getPlayerMove()
+        puntos = juego(usuario, gameStatus, startTime)
         puntuacion(puntos, gameStatus, plyr)
         if SAVE_ON_CYCLE:
             guardar(gameStatus)
--- mensajes.py
+++ mensajes.py
@@ -1,11 +1,13 @@
 msg = {
     "inicio": "\nBienvenido al juego de Piedra papel, tijera, lagarto o spock\n\n",
-    "input": "Escriba su elección: Piedra(R), Papel(P), Tijeras(S), Lagarto(L) o Spock(K): ",
+    "nameInput": "Introduzca su nombre: ",
+    "getPlayerMove": "Escriba su elección: Piedra(R), Papel(P), Tijeras(S), Lagarto(L) o Spock(K): ",
     "error": " ERROR  Introduzca una letra valida\n",
     "exit": "\nPara salir del juego introduzca Exit(E)\n",
     "linea": "------------------------------------------------------------------------------\n",
     "jugador": "\n{plyr}:",
-    "tiempo": "El programa lleva {tiempoConvertido} en ejecucion",
+    "sessionTime": "El programa lleva {sessionTime} en ejecucion",
+    "totalTime": "El programa se ha ejecutado {tiempoTotal} en total",
     "crear": "Se está creando su partida\n",
     "cargar": "Se está cargarndo su partida\n",
     "moveR": "Piedra",
@@ -14,12 +16,12 @@ msg = {
     "moveL": "Lagarto",
     "moveK": "Spock",
     "mensajeSalida": "Hasta la vista",
-    "mensajeMaquina": "\nLa maquina eligió {eleccionMaquina}",
-    "mensajeVictoria": "Has ganado, {eleccionUsuario} gana a {eleccionMaquina}",
-    "mensajeDerrota": "Has perdido, {eleccionUsuario} pierde contra {eleccionMaquina}",
-    "mensajeEmpate": "Has empatado, {eleccionUsuario} empata con {eleccionMaquina}",
-    "contador": "Llevas {Partidas} partidas \n{Victorias} ganadas \n{Derrotas} perdidas \n{Empates} empatadas",
-    "stats":  "Has introducido un total de {total_imput} y solo {buenas_imput} han sido aceptadas por el programa"
+    "mensajeMaquina": "\nLa maquina eligió {cpuChoice}",
+    "mensajeVictoria": "Has ganado, {userChoice} gana a {cpuChoice}",
+    "mensajeDerrota": "Has perdido, {userChoice} pierde contra {cpuChoice}",
+    "mensajeEmpate": "Has empatado, {userChoice} empata con {cpuChoice}",
+    "contador": "Llevas {partidas} partidas \n{victorias} ganadas \n{derrotas} perdidas \n{empates} empatadas",
+    "stats":  "Has introducido un total de {total_input} y solo {buenas_input} han sido aceptadas por el programa"
 
 
 }
